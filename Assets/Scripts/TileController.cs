8782ed9e36f33b22439285ba45542dfe778c89d5
Felix Siebeneicker <felix.sieben@t-online.de>
2019-10-06 16:10:10 +0200

    Adding tile states

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -6,7 +6,43 @@ using UnityEngine;
 public class TileController : MonoBehaviour
 {
     public Vector2Int coordinates;
-    public Action<Vector2Int> OnTileInteracted;
+    public Action<Vector2Int> OnTileInteracted = delegate { };
+    public Color colorDirt;
+    public Color colorGrass;
+    public GameObject tileGraphics;
+    public GameObject plusGraphics;
+
+    public enum TileState { Empty, Plus, Tile }
+    public TileState tileState;
+
+    private void Start()
+    {
+        tileState = (TileState)UnityEngine.Random.Range(0, 3);
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        if (tileState == TileState.Empty)
+        {
+            tileGraphics.SetActive(false);
+            plusGraphics.SetActive(false);
+        }
+        else if (tileState == TileState.Plus)
+        {
+            tileGraphics.SetActive(false);
+            plusGraphics.SetActive(true);
+        }
+        else if (tileState == TileState.Tile)
+        {
+            tileGraphics.SetActive(true);
+            plusGraphics.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("Invalid tile state!");
+        }
+    }
 
     private void OnMouseEnter()
     {
